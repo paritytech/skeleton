530b4e23e1131d551a092240d151cce0f7c60c2d
Gav <gavin@parity.io>
2017-01-26 18:35:29 +0100

    Add oo7.

--- src/client/scripts/entry.jsx
+++ src/client/scripts/entry.jsx
@@ -1,6 +1,16 @@
-import 'jquery';
 import React from 'react';
 import {render} from 'react-dom';
+import {setDefaultTransformBondContext} from 'oo7';
+import {setupBonds} from 'oo7-parity';
 import {App} from './app.jsx'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-render(<App/>, document.getElementById('app'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+// We use and dirty up the global namespace here.
+parity.bonds = setupBonds(parity.api);
+
+render(<MuiThemeProvider><App/></MuiThemeProvider>, document.getElementById('app'));
