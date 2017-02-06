e7731cfaa4e999113398ae81a23bf2f2877db3c3
Gav <gavin@parity.io>
2017-02-06 16:48:49 -0500

    Remove unneeded code.

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import {Rspan, TextBond} from 'oo7-react';
-import {Bond} from 'oo7';
 import styles from "../style.css";
 
 export class App extends React.Component {
-	constructor() {
-		super();
-		this.bond = new Bond;
-	}
 	render() {
 		return (
-			<div>
-				<TextBond bond={this.bond}/>
-				<Rspan>{parity.bonds.registry.lookupAddress(this.bond, 'A')}</Rspan>
-			</div>
+			<div>Hello world!</div>
 		);
 	}
 }
