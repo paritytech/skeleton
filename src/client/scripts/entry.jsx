ae696f0dd9509f12d01d0bf915bc2c48786693a6
Gav Wood <gavin@parity.io>
2017-06-04 20:51:32 +0200

    Remove unnecessary line

--- src/client/scripts/entry.jsx
+++ src/client/scripts/entry.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {setupBonds} from 'oo7-parity';
-
 import {App} from './app.jsx'
-
-// We use and dirty up the global namespace here.
-parity.bonds = setupBonds(parity.api);
 
 render(<App/>, document.getElementById('app'));
